08b6a73fbf04014bb08d6523c0f413b0c3cf3745
leslie101689 <leslie101689@hotmail.com>
2020-10-27 07:41:32 +0800

    Adding Animation to Websites

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -6,7 +6,10 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function() {
 
       var buttonInnerHTML = this.innerHTML;
+
       makeSound(buttonInnerHTML);
+
+      buttonAnimation(buttonInnerHTML);
   });
 
 }
@@ -14,6 +17,8 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
 // Detecting Keyboard Press
 document.addEventListener("keypress", function(event) {
   makeSound(event.key);
+
+  buttonAnimation(event.key);
 });
 
 function makeSound(key) {
@@ -56,4 +61,12 @@ function makeSound(key) {
     default:
       console.log();
   }
+}
+
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function() {
+    activeButton.classList.remove("pressed");
+  }, 100);
 }
