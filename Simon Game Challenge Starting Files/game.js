105ecb6b55f0a78c09dbd53af735a5226fc676d5
leslie101689 <leslie101689@hotmail.com>
2020-11-06 07:33:16 +0800

    Step 4 - Check which Button is Pressed

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -2,6 +2,16 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
 
+var userClickedPattern = [];
+
+$(".btn").click(function() {
+  var userChosenColour = $(this).attr("id");
+
+  userClickedPattern.push(userChosenColour);
+
+  // console.log(userClickedPattern);
+});
+
 function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4); // 0-3
 
