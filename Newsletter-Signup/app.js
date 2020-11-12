af0e906bbcfdac9d494262dd414572e4233571fb
leslie101689 <leslie101689@hotmail.com>
2020-11-13 07:38:02 +0800

    11.Posting data to mailchimp server via their API

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
 
 const app = express();
 
@@ -14,13 +15,50 @@ app.get("/", function(req, res) {
 });
 
 app.post("/",function(req, res) {
-  var firstName = req.body.fname;
-  var lastName = req.body.lname;
-  var email = req.body.email;
+  const firstName = req.body.fname;
+  const lastName = req.body.lname;
+  const email = req.body.email;
 
-  console.log(firstName, lastName, email);
+  var data = {
+    members: [
+      {
+        email_address: email,
+        status:"subscribed",
+        merge_fields:{
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+
+  const jsonData = JSON.stringify(data)
+
+  const url = "https://us2.api.mailchimp.com/3.0/lists/7c8cd24dd4";
+
+  const options = {
+    method:"POST",
+    auth:"leslie1:256da4b95f04579b7cb2279485df50a1-us2"
+  }
+
+  const request = https.request(url, options, function(response) {
+    response.on("data", function(data) {
+      console.log(JSON.parse(data));
+    })
+  })
+
+  request.write(jsonData);
+  request.end();
 });
 
 app.listen(3000, function() {
   console.log("Server is running on port 3000");
 });
+
+'{"name":"","contact":{"company":"","address1":"","address2":"","city":"","state":"","zip":"","country":"","phone":""},"permission_reminder":"","use_archive_bar":false,"campaign_defaults":{"from_name":"","from_email":"","subject":"","language":""},"notify_on_subscribe":"","notify_on_unsubscribe":"","email_type_option":false,"visibility":"pub","double_optin":false,"marketing_permissions":false}'
+
+// API Key
+// 256da4b95f04579b7cb2279485df50a1-us2
+
+// List
+// 7c8cd24dd4
