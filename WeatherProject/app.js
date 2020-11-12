67515f3fc80303252ef706111c0ca14108cc49c6
leslie101689 <leslie101689@hotmail.com>
2020-11-12 20:54:07 +0800

    8.Using Body Parser to Parse POT req to server

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.get("/", function(req,res) {
+app.use(bodyParser.urlencoded({extended:true}));
 
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=5e6795f34c43a1af13a04d517c610b9c"
+app.get("/", function(req,res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res) {
+  const query = req.body.cityName;
+  const apiKey = "5e6795f34c43a1af13a04d517c610b9c";
+  const units = "metric";
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&units=" + units + "&appid=" + apiKey;
   https.get(url, function(response) {
     console.log(response.statusCode);
 
@@ -16,15 +25,35 @@ app.get("/", function(req,res) {
       const icon = weatherData.weather[0].icon
       const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
       res.write("<p>The weather is currently " + weatherDesc + "</p>");
-      res.write("<h1>The temperature in London is " + temp + " degress Celcius.</h1>");
+      res.write("<h1>The temperature in " + query + " is " + temp + " degress Celcius.</h1>");
       res.write("<img src=" + imageURL + ">")
       res.send();
     })
-  })
+  });
 
-  // res.send("Server is up and running.");
+  // console.log("Post request received.")
 });
 
 app.listen(3000, function() {
   console.log("Server is running on port 3000");
-})
+});
+
+// const query = "London";
+// const apiKey = "5e6795f34c43a1af13a04d517c610b9c";
+// const units = "metric";
+// const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&units=" + units + "&appid=" + apiKey;
+// https.get(url, function(response) {
+//   console.log(response.statusCode);
+//
+//   response.on("data", function(data) {
+//     const weatherData = JSON.parse(data)
+//     const temp = weatherData.main.temp
+//     const weatherDesc = weatherData.weather[0].description
+//     const icon = weatherData.weather[0].icon
+//     const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+//     res.write("<p>The weather is currently " + weatherDesc + "</p>");
+//     res.write("<h1>The temperature in London is " + temp + " degress Celcius.</h1>");
+//     res.write("<img src=" + imageURL + ">")
+//     res.send();
+//   })
+// });
