5dd1cb39df177ec9ad8f34d085ad3e93b7adad1c
leslie101689 <leslie101689@hotmail.com>
2020-11-13 06:54:39 +0800

    10. Setting up the Sign Up Page

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -4,8 +4,21 @@ const request = require("request");
 
 const app = express();
 
+// to get local folder
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", function(req, res) {
-  res.send("Server is up and running");
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/",function(req, res) {
+  var firstName = req.body.fname;
+  var lastName = req.body.lname;
+  var email = req.body.email;
+
+  console.log(firstName, lastName, email);
 });
 
 app.listen(3000, function() {
