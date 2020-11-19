3b438bf282a769e99c0ae4484f01e5e75939335c
leslie101689 <leslie101689@hotmail.com>
2020-11-19 08:13:14 +0800

    10.Understand node module export how to pass function and data

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -2,11 +2,12 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
-let items = ["Buy Food", "Cook Food", "Eat Food"];
-let workItems = [];
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
 
 app.set('view engine', 'ejs');
 
@@ -17,15 +18,7 @@ app.use(express.static("public"));
 
 app.get("/", function(req, res) {
 
-  let today = new Date();
-
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  };
-
-  let day = today.toLocaleDateString("en-US", options);
+  const day = date.getDate();
 
   res.render('list', {
     listTitle: day,
@@ -35,7 +28,7 @@ app.get("/", function(req, res) {
 
 app.post("/", function(req, res) {
 
-  let item = req.body.newItem;
+  const item = req.body.newItem;
 
   if(req.body.list === "Work") {
     workItems.push(item);
@@ -51,7 +44,7 @@ app.get("/work", function(req,res){
 });
 
 app.post("/work", function(req,res) {
-  let item = req.body.newItem;
+  const item = req.body.newItem;
   items.push(item);
   res.redirect("/work");
 });
