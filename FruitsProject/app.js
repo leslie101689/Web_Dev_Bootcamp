8b179ed82555df9afd18dc1248ed4f58a4c3a134
leslie101689 <leslie101689@hotmail.com>
2021-01-07 07:13:06 +0800

    3.Data Validation with Mongoose

--- FruitsProject/app.js
+++ FruitsProject/app.js
@@ -6,20 +6,27 @@ const mongoose = require('mongoose');
 mongoose.connect("mongodb://localhost:27017/fruitsDB", { useNewUrlParser: true, useUnifiedTopology: true });
 
 const fruitSchema = new mongoose.Schema ({
-  name: String,
-  rating: Number,
+  name: {
+    type: String,
+    required: [true, "Please check your data entry, no name specified!"]
+  },
+  rating: {
+    type: Number,
+    min: 1,
+    max: 10
+  },
   review: String
 });
 
 const Fruit = mongoose.model("Fruit", fruitSchema);
 
 const fruit = new Fruit({
-  name: "Apple",
-  rating: 7,
+  // name: "Apple",
+  rating: 34,
   review: "Pretty solid as a fruit."
 });
 
-// fruit.save();
+fruit.save();
 
 const personSchema = new mongoose.Schema ({
   name: String,
@@ -35,23 +42,23 @@ const person = new Person ({
 
 // person.save();
 
-const kiwi = new Fruit({
-  name: "Kiwi",
-  score: 10,
-  review: "The best fruit"
-});
-
-const orange = new Fruit({
-  name: "Orange",
-  score: 4,
-  review: "Too Sour for me"
-});
-
-const banana = new Fruit({
-  name: "Banana",
-  score: 3,
-  review: "Weird texture"
-});
+// const kiwi = new Fruit({
+//   name: "Kiwi",
+//   score: 10,
+//   review: "The best fruit"
+// });
+//
+// const orange = new Fruit({
+//   name: "Orange",
+//   score: 4,
+//   review: "Too Sour for me"
+// });
+//
+// const banana = new Fruit({
+//   name: "Banana",
+//   score: 3,
+//   review: "Weird texture"
+// });
 
 // Fruit.insertMany([kiwi, orange, banana], function(err) {
 //   if (err) {
